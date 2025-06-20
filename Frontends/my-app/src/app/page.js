25145a5f27050bd52a99f8c728125a8aff0cf37c
Mohammad Danish <m.danish00707@gmail.com>
2025-06-20 19:37:54 +0530

    Added

--- Frontends/my-app/src/app/page.js
+++ Frontends/my-app/src/app/page.js
@@ -1,7 +1,7 @@
 import { Suspense } from 'react'
 import MyComponent from '@/components/HomeContent' // or whatever the component is called
 import './globals.css'
-export default function Page() {
+export default function Home() {
   return (
     <main>
       <Suspense fallback={<div>Loading...</div>}>
